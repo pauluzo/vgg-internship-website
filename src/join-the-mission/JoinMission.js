c3993cbdc050e084e8cbf6d2a74829d8422c9f74
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-05-24 23:38:25 +0100

    skeletal Html

--- src/join-the-mission/JoinMission.js
+++ src/join-the-mission/JoinMission.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './JoinMission.css';
 
 export default function JoinMission() {
   return (
@@ -12,6 +13,64 @@ export default function JoinMission() {
           accusantium.
           <button>join now</button>
         </div>
+      </section>
+      <section className='learningTracks'>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <div className='tracks'>
+          <img src='' alt='' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at
+            minima alias aliquid quia! Recusandae consequatur laborum aut quod
+            eum odio quibusdam rem. Explicabo, sapiente eius non temporibus
+            fugiat odio!
+          </p>
+        </div>
+        <div className='tracks'>
+          <img src='' alt='' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at
+            minima alias aliquid quia! Recusandae consequatur laborum aut quod
+            eum odio quibusdam rem. Explicabo, sapiente eius non temporibus
+            fugiat odio!
+          </p>
+        </div>
+        <div className='tracks'>
+          <img src='' alt='' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at
+            minima alias aliquid quia! Recusandae consequatur laborum aut quod
+            eum odio quibusdam rem. Explicabo, sapiente eius non temporibus
+            fugiat odio!
+          </p>
+        </div>
+        <div className='tracks'>
+          <img src='' alt='' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at
+            minima alias aliquid quia! Recusandae consequatur laborum aut quod
+            eum odio quibusdam rem. Explicabo, sapiente eius non temporibus
+            fugiat odio!
+          </p>
+        </div>
+      </section>
+      <section className='interns'>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id quidem
+          obcaecati iste aspernatur vero excepturi, eaque vel molestiae enim
+          eius quis! Ab cumque impedit aspernatur ea expedita voluptates
+          doloribus nihil?
+        </p>
+        <aside>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad provident
+          assumenda iusto dolore consectetur ex.
+        </aside>
+      </section>
+      <section className='getStarted'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est suscipit
+        architecto nulla quae esse necessitatibus adipisci veniam! Optio
+        voluptates vel odit iure placeat quia nisi, non, laudantium eaque enim
+        alias.
+        <button>Join Now</button>
       </section>
     </div>
   );
