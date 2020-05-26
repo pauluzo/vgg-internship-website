0e3c30d1bd1de06876d0f0386bfcb08b28091e69
pauluzo <45356331+pauluzo@users.noreply.github.com>
2020-05-26 01:02:50 +0100

    second git flow testing

--- src/become-an-intern/SurveyPage.js
+++ src/become-an-intern/SurveyPage.js
@@ -5,6 +5,7 @@ export default function SurveyPage() {
   return(
     <div>
       <p>No changes yet. I am testing that the git flow works properly</p>
+      <p>Testing again, that this works well. Nothing special here</p>
     </div>
   )
 }
