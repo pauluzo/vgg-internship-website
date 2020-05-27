7875c7fdfb955b081e0e4334220a03aafb0f4401
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-05-26 20:18:01 -0700

    add past-intern to the and data

---
+++ src/InternData/internData.js
@@ -0,0 +1,32 @@
+export const internData = [
+    {
+        id: 1,
+        name: "",
+        track : "",
+        image_url:""
+    },
+    {
+        id: 1,
+        name: "",
+        track : "",
+        image_url:""
+    },
+    {
+        id: 1,
+        name: "",
+        track : "",
+        image_url:""
+    },
+    {
+        id: 1,
+        name: "",
+        track : "",
+        image_url:""
+    },
+    {
+        id: 1,
+        name: "",
+        track : "",
+        image_url:""
+    }
+];
---
+++ src/past-intern/pastIntern.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class pastIntern extends Component {
+    render() {
+        return (
+            <div className="container">
+                
+            </div>
+        );
+    }
+}
+
+export default pastIntern;
