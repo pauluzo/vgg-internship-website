719a3d492ac0b75dde4bc9874a032b19d9b83a4b
pauluzo <45356331+pauluzo@users.noreply.github.com>
2020-05-28 07:58:04 +0100

    survey ui design completed.

--- src/become-an-intern/SurveyPage.js
+++ src/become-an-intern/SurveyPage.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "./images/intro-img.png";
+import CloseBtn from "./images/close-icon.png"
 import "./SurveyPage.css";
 
 export default function SurveyPage() {
+  // You'll probably want to delete this, just a placeholder
+  const questionHolder = `
+    Sed ut perspiciatis unde omnis iste natus error sit 
+    voluptatem accusantium doloremque laudantium.
+  `;
+
+  const [question, setQuestion] = useState(questionHolder);
+  // I'm not sure how you'd like to implement this. You can change it.
+  const [options, setOptions] = useState(["Not Quite", "Somewhat", "Very"]);
 
   return(
     <div className="survey-page-container" >
       <div className="survey-page-body">
+        <div className="close-btn">
+          <img alt="close-btn" src={CloseBtn} />
+        </div>
         <div className="survey-intro">
           <div className="intro-layer">
             <div className="intro-container">
-              <img alt="vgg-img" src={Image}></img>
+              <div className="img-container">
+                <img alt="vgg-img" src={Image}></img>
+              </div>
               <div className="intro-text">
                 <span className="span-1">Survey</span>
                 <span className="span-2">Questionnaire</span>
@@ -18,8 +33,15 @@ export default function SurveyPage() {
             </div>
           </div>
         </div>
-        <div>
-
+        <div className="survey-question">
+          <div className="question-container">
+            <div style={{maxWidth: "300px"}}>{question}</div>
+            <div className="button-container">
+              <button className="option-btn" onClick={() => {}}>{options[0]}</button>
+              <button className="option-btn" onClick={() => {}}>{options[1]}</button>
+              <button className="option-btn" onClick={() => {}}>{options[2]}</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
