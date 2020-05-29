5e3809bdb6935c466fe7cc7a608808885aeab279
Hameedah <adebanjohameedah7@gmail.com>
2020-05-29 23:36:45 +0100

    Revert "Feature/become an intern"

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import JoinMission from './join-the-mission/JoinMission';
-import SurveyPage from './become-an-intern/SurveyPage';
-import { RegistrationForm } from './become-an-intern/RegistrationForm';
-
 export class Router extends Component {
   render() {
     return (
       <>
-        <Route component={RegistrationForm} path='/registration-form' />
-        <Route component={SurveyPage} path='/take-a-survey' />
-        <Route component={JoinMission} path='/join-the-mission' />
+        <Route component={JoinMission} path='/mission' />
       </>
     );
   }
