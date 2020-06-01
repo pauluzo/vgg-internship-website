5e895c13f025899340850fd455d2245c6b530eb6
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-05-31 18:22:51 -0700

    add some more intern data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Router from './Router';
 import { BrowserRouter as Route, Switch } from 'react-router-dom';
-import PastIntern from './past-intern/pastIntern';
+
 
 
 
