40ddbaf76494b9a8fb362c5ae20df9d08d4f7f21
pauluzo <45356331+pauluzo@users.noreply.github.com>
2020-06-10 14:08:03 +0100

    fixed nav toggle bug

--- src/home-page/Navbar.js
+++ src/home-page/Navbar.js
@@ -6,12 +6,12 @@ import { Nav, Navbar} from "react-bootstrap";
 
  const NavBar=() => {
   return (
-    <Navbar collapseOnSelect variant="dark" expand="lg" className="background" style={{position: "fixed", width: "100%", zIndex: 1, height: "70px"}}>
+    <Navbar collapseOnSelect variant="dark" expand="lg" className="background" style={{position: "fixed", width: "100%", zIndex: "2", height: "70px"}}>
       <Navbar.Brand href="/">
       <img  style={{width:"200px", textAlign: "center"}}  src="https://res.cloudinary.com/dev-sam/image/upload/v1590685777/we7l1ttvgs6btbvuilvt.png" alt="logo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{color: "white"}}/>
-      <Navbar.Collapse id="responsive-navbar-nav">
+      <Navbar.Collapse id="responsive-navbar-nav" style={{zIndex: "1", backgroundColor: "inherit"}}>
         <Nav className="ml-auto pointer">
           <Nav.Link className="text-white text" href="/">Home</Nav.Link>
           <Nav.Link className="text-white text" href="/join-the-mission">Join The Mission</Nav.Link>
