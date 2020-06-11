30e6c5922b74eb5bd276c325d4e3490433bb7274
Paul.O <45356331+pauluzo@users.noreply.github.com>
2020-06-11 03:57:21 +0100

    Revert "changes to the ui of the facilitators page"

--- src/components/Facilitators.js
+++ src/components/Facilitators.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import FacilitatorItems from './FacilitatorItems';
-import facilitatorDetails from './facilitatorsdata';
-import './facilitatorStyle.css';
-import CommunityManager from './communityManager';
+import React from "react";
+import FacilitatorItems from "./FacilitatorItems";
+import facilitatorDetails from "./facilitatorsdata";
+import "./facilitatorStyle.css";
+import CommunityManager from "./communityManager"
 import NavBar from "../home-page/Navbar"
 import Footer from "../home-page/Footer"
 
+
+
 const Facilitators = () => {
   return (
-   <>
-    <NavBar/>
-    <div>
-      <div style={{ width: '100%', height: '90vh' }}>
-        <div className='facilitators-intro'>
-          <div>
-            <span style={{ color: '#DAA520', fontWeight: 'bold' }}>
-              Our Facilitators
-            </span>
-            <h1>
-              "We never know which lives we influence,
-              <br /> when, or why! We{' '}
-              <span style={{ color: '#DAA520' }}>JUST DO IT</span>"
-            </h1>
+    <>
+      <NavBar/>
+      <div>
+        <div style={{width: "100%", height: "90vh", paddingTop: "70px"}}>
+          <div className="facilitators-intro">
+            <div >
+              <span style={{color: "#DAA520", fontWeight: "bold"}}>Our Facilitators</span>
+              <h1 >"We never know which lives we influence,<br/> when, or why! We <span style={{color: "#DAA520"}}>JUST DO IT</span>"</h1>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <CommunityManager />
-      </div>
-      <div className='cards-container' style={{ backgroundColor: 'gainsboro' }}>
-        <h2>Meet the Team</h2>
-        <div className='facilitators-container'>
+        <div>
+          <CommunityManager />
+        </div>
+        <div style={{width: "100%", height: "300px"}}>
+          <div className="transforming"></div>
+        </div>
+        <div className="cards-container" style={{backgroundColor: "gainsboro"}}>
           {facilitatorDetails.map((facilitator) => {
             return (
               <div key={facilitator.id}>
